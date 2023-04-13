82a60abcb5620edec6a1c99120435bd5d1de0212
Leo Carpenter <leo.carpenter.pro@gmail.com>
2023-04-13 11:42:33 +0200

    set default terminal to alacritty

--- rc.lua
+++ rc.lua
@@ -58,8 +58,7 @@ beautiful.font      = "sans 13"
 beautiful.wallpaper = "/home/leo/.config/awesome/wall_rainbow_cheatsheet.png"
 
 -- This is used later as the default terminal and editor to run.
--- terminal = "x-terminal-emulator"
-terminal = "gnome-terminal"
+terminal = "alacritty"
 editor = os.getenv("EDITOR") or "editor"
 editor_cmd = terminal .. " -e " .. editor
 
